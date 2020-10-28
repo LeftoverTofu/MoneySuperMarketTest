968e6278b3c4fdabd040fe45be8d14d445f921d7
Soi-Phun Chuong <leftovertofugames@gmail.com>
2020-10-28 17:28:11 +0000

    Added Spring Boot application and enabling caching on main class (App.java)

--- src/main/java/com/moneysupermarket/app/App.java
+++ src/main/java/com/moneysupermarket/app/App.java
@@ -1,10 +1,15 @@
 package com.moneysupermarket.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SpringApplication.run(App.class, args);
 	}
 
 }
