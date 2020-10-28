e8ede63b7a70cc5b6ecb67e35610aeec97ff5efd
Soi-Phun Chuong <leftovertofugames@gmail.com>
2020-10-28 17:48:26 +0000

    Added RestTemplate to BitcoinExchangeServiceImpl to request to the API

--- src/main/java/com/moneysupermarket/app/service/impl/BitcoinExchangeServiceImpl.java
+++ src/main/java/com/moneysupermarket/app/service/impl/BitcoinExchangeServiceImpl.java
@@ -2,6 +2,7 @@ package com.moneysupermarket.app.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.moneysupermarket.app.repository.BitcoinExchangeRepository;
 import com.moneysupermarket.app.service.BitcoinExchangeService;
@@ -10,10 +11,12 @@ import com.moneysupermarket.app.service.BitcoinExchangeService;
 public class BitcoinExchangeServiceImpl implements BitcoinExchangeService {
 
 	private BitcoinExchangeRepository bitcoinExchangeRepository;
+	private RestTemplate restTemplate;
 	
 	@Autowired
-	public BitcoinExchangeServiceImpl(BitcoinExchangeRepository bitcoinExchangeRepository) {
+	public BitcoinExchangeServiceImpl(BitcoinExchangeRepository bitcoinExchangeRepository, RestTemplate restTemplate) {
 		this.bitcoinExchangeRepository = bitcoinExchangeRepository;
+		this.restTemplate = restTemplate;
 	}
 
 }
