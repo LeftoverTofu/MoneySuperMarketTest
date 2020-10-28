f10a0be5606c943fd14c85615844f4f0bcc5d9b6
Soi-Phun Chuong <leftovertofugames@gmail.com>
2020-10-28 17:23:27 +0000

    Added BitCoinExchangeService with the implementation class. Added the test class for the service, added 2 initial fail test cases to call 2 API from block chain and Exmo

---
+++ src/main/java/com/moneysupermarket/app/service/impl/BitcoinExchangeServiceImpl.java
@@ -0,0 +1,10 @@
+package com.moneysupermarket.app.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.moneysupermarket.app.service.BitcoinExchangeService;
+
+@Service
+public class BitcoinExchangeServiceImpl implements BitcoinExchangeService {
+
+}
